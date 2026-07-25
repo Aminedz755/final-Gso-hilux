e7799ee2856fc11241fdd5a5e4755baa6dcfd088
Aminedz755 <jokeramine411@gmail.com>
2026-07-25 11:01:13 +0400

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ const vehicles = [
             manufacturer: "Toyota Motor Corporation",
 
             vehicleName: "TOYOTA HILUX",
-            vehicleDescription: "(GUN122) 2.4L Pick-up 4WD 4 Doors",
+            vehicleDescription: "(GUN122) 2.4L Pick-up RWD 4Doors",
 
             category: "Light Truck",
             modelYear: "2026",
