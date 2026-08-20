bcc26f143274c94ee444b245d38eebc8472072f2
Aminedz755 <jokeramine411@gmail.com>
2026-08-20 12:11:19 +0400

    Refactor vehicle database and improve routing logic

--- script.js
+++ script.js
@@ -6,6 +6,74 @@
 // ===============================
 
 const vehicles = [
+     {
+        route: "/509189",
+
+        data: {
+            ccrNumber: "509189",
+            approvedDate: "27 November 2025",
+
+            manufacturer: "Toyota Motor Corporation",
+
+            vehicleName: "TOYOTA HILUX",
+            vehicleDescription: "(GUN122) 2.4L Pick-up 4WD 4 Doors",
+
+            category: "Light Truck",
+            modelYear: "2025",
+            country: "SOUTH AFRICA",
+
+            productionMonth: "11",
+            productionYear: "2025",
+
+            vin: "AHTKB8CD*0*******",
+
+            maxWeight: "2810 kg",
+            curbWeight: "1800 kg",
+
+            frontAxle: "1200 kg",
+            rearAxle: "1750 kg",
+
+            length: "5330 mm",
+            width: "1800 mm",
+            height: "1700 mm",
+
+            wheelbase: "3085 mm",
+
+            frontTrack: "1510 mm",
+            rearTrack: "1510 mm",
+
+            bodyType: "Chassis Frame",
+
+            passengers: "5 (including the driver)",
+
+            engineType: "Diesel",
+            cylinders: "4",
+            displacement: "2393 cc",
+            airIntake: "Turbo",
+
+            enginePower: "110 kW at 3400 rpm",
+
+            pollutantLimit: "Comply with Euro5",
+
+            transmission: "Automatic",
+
+            ecall: "Not Provided",
+
+            serviceBrakes: "Hydraulic",
+
+            emergencyBrakes:
+                "Combined with the service brake",
+
+            vehicleClass: "Light Truck",
+
+            fuelEconomy: "15.9 km/L",
+
+            fuelRating: "Excellent",
+
+            additionalInformation:
+                "Also comply with the National regulations for member countries mentioned in the Annex of the list of Technical Regulations for MV 2026MY-D3, when exporting to those countries. The seating reference point of the lowest seat exceeds 700mm from the ground. This vehicle type complies with ECE13. This vehicle is only for Saudi Arabia, Kuwait and Qatar market."
+        }
+    },
 
     {
         route: "/509192",
@@ -347,73 +415,6 @@ const vehicles = [
 
             additionalInformation:
                 "This vehicle complies with applicable Gulf technical regulations and safety standards."
-        }
-    }, {
-        route: "/509189",
-
-        data: {
-            ccrNumber: "509189",
-            approvedDate: "27 November 2025",
-
-            manufacturer: "Toyota Motor Corporation",
-
-            vehicleName: "TOYOTA HILUX",
-            vehicleDescription: "(GUN122) 2.4L Pick-up 4WD 4 Doors",
-
-            category: "Light Truck",
-            modelYear: "2025",
-            country: "SOUTH AFRICA",
-
-            productionMonth: "11",
-            productionYear: "2025",
-
-            vin: "AHTKB8CD*0*******",
-
-            maxWeight: "2810 kg",
-            curbWeight: "1800 kg",
-
-            frontAxle: "1200 kg",
-            rearAxle: "1750 kg",
-
-            length: "5330 mm",
-            width: "1800 mm",
-            height: "1700 mm",
-
-            wheelbase: "3085 mm",
-
-            frontTrack: "1510 mm",
-            rearTrack: "1510 mm",
-
-            bodyType: "Chassis Frame",
-
-            passengers: "5 (including the driver)",
-
-            engineType: "Diesel",
-            cylinders: "4",
-            displacement: "2393 cc",
-            airIntake: "Turbo",
-
-            enginePower: "110 kW at 3400 rpm",
-
-            pollutantLimit: "Comply with Euro5",
-
-            transmission: "Automatic",
-
-            ecall: "Not Provided",
-
-            serviceBrakes: "Hydraulic",
-
-            emergencyBrakes:
-                "Combined with the service brake",
-
-            vehicleClass: "Light Truck",
-
-            fuelEconomy: "15.9 km/L",
-
-            fuelRating: "Excellent",
-
-            additionalInformation:
-                "Also comply with the National regulations for member countries mentioned in the Annex of the list of Technical Regulations for MV 2026MY-D3, when exporting to those countries. The seating reference point of the lowest seat exceeds 700mm from the ground. This vehicle type complies with ECE13. This vehicle is only for Saudi Arabia, Kuwait and Qatar market."
         }
     },
      {
